4ed2da69c4862678ec229a7ef9d4de128506208a
DiuTTNguyen <thanhdiu.nguyen@gmail.com>
2021-10-20 09:56:20 -0400

    Update funding.js

--- src/pages/funding.js
+++ src/pages/funding.js
@@ -12,6 +12,7 @@ export default function funding() {
             </div>
             <div className="width-100 flex-column text-center">
                 <p>“Targeting Post-transcriptional Regulators to Eradicate Leukemic Stem Cells in Acute Myeloid Leukemia”. 1,258,197GBP. 2022-2028</p>
+                <p>"American Society of Hematology Scholar Award". $100,000. 2021-2023</p>
             </div>
             </div>
         </Layout>
