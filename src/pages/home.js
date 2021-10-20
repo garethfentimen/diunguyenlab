0bc3592122367ccba8d301187cd4d86a0d340eda
Gareth Fentimen <garethfentimen@gmail.com>
2021-10-20 10:30:33 -0400

    new image for home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,43 @@
-import * as React from "react";
-import { graphql } from 'gatsby';
+import React, { useEffect } from "react";
+import { graphql, Link } from 'gatsby';
 import './home.scss';
-import expImg2 from '../images/shLuc11_exp_1_edit.jpg';
+import expImg2 from '../images/shLuc11_exp_1_text_crop.jpg';
 
-const home = ({data}) => {
-    console.log(data);
+const Home = ({data}) => {
+
+    useEffect(() => {
+      if (typeof window !== 'undefined') {
+        const observer = new IntersectionObserver(entry => {  
+          // We will fill in the callback later...
+          if (entry.isIntersecting) {
+            // It's visible. Add the animation class here!
+            entry.target.classList.add('animation');
+          }
+        });
+        
+        // Tell the observer which elements to track
+        observer.observe(document.querySelector('.exp-images'));
+      }
+    });
+
     return (
-        <div className="exp-images">
-          <div className="banner">
+        <div>
+          <div className="exp-images">
+          {/* <div className="banner">
             <span className="welcome">Welcome</span> 
             <span className="tothe">to the</span>
             <span className="dn-lab">Diu Nguyen</span>
             <span className="lab">Lab</span>
-          </div>
-          <img alt="lab experiment" className={`exp-img`} src={expImg2} />
+          </div> */}
+          <img alt="Welcome to the Diu Nguyen lab: superimposed on an image from a microscope" className={`exp-img`} src={expImg2} />
         </div>
+        <div className="content">
+          <p>
+            <Link className="btn" to={`/funding`}>Funding</Link>
+            <button className="btn">About us</button>
+          </p>
+        </div>
+      </div>
     )
 };
 
@@ -28,4 +51,4 @@ export const query = graphql`
   }
 `;
 
-export default home;
+export default Home;
