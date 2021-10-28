f6690134811e282f42e4b863f7e7b5a3a6a92afa
DiuTTNguyen <thanhdiu.nguyen@gmail.com>
2021-10-28 12:21:11 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,12 +6,21 @@ export default function contact() {
     const showTick = false;
     return (
         <Layout>
-    <div className="flex" role="button">
+              <div className="flex-column">
+                    <div className="flex" role="button">
                             <p tabIndex={0} style={{width: 'fit-content', padding: '.1rem 0.5rem', cursor: 'pointer', boxShadow: '1px 1px 1px 1px #001d5826'}} onClick={onClickEmail} onKeyDown={onClickEmail}>
                                 <span id="email">thanhdiu.nguyen@gmail.com</span>
                             </p>
                             {showTick && <div className="marg-left-lg flex"><span className="green-check"></span><p>Copied to clipboard</p></div>}
-                        </div>         
+                        </div>
+                        <div>or</div>     
+                        <div className="flex" role="button">
+                            <p tabIndex={0} style={{width: 'fit-content', padding: '.1rem 0.5rem', cursor: 'pointer', boxShadow: '1px 1px 1px 1px #001d5826'}} onClick={onClickEmail} onKeyDown={onClickEmail}>
+                                <span id="email">nguyend@mskcc.org</span>
+                            </p>
+                            {showTick && <div className="marg-left-lg flex"><span className="green-check"></span><p>Copied to clipboard</p></div>}
+                        </div>      
+              </div>
         </Layout>
     )
 }
