5fbcc7da77b5e58b7d9e57951daa4ba169720148
DiuTTNguyen <thanhdiu.nguyen@gmail.com>
2021-10-28 12:16:49 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,9 +3,15 @@ import Layout from '../components/layout/layout';
 import './contact.scss';
 
 export default function contact() {
+    const showTick = false;
     return (
         <Layout>
-            
+    <div className="flex" role="button">
+                            <p tabIndex={0} style={{width: 'fit-content', padding: '.1rem 0.5rem', cursor: 'pointer', boxShadow: '1px 1px 1px 1px #001d5826'}} onClick={onClickEmail} onKeyDown={onClickEmail}>
+                                <span id="email">thanhdiu.nguyen@gmail.com</span>
+                            </p>
+                            {showTick && <div className="marg-left-lg flex"><span className="green-check"></span><p>Copied to clipboard</p></div>}
+                        </div>         
         </Layout>
     )
 }
