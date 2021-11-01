1574248791211785ca0a64cef10903d0ef764fe3
DiuTTNguyen <thanhdiu.nguyen@gmail.com>
2021-10-31 23:33:20 -0400

    Update funding.js

--- src/pages/funding.js
+++ src/pages/funding.js
@@ -10,10 +10,11 @@ export default function funding() {
             <div className="marg-top-sm width-100" style={{width: "20rem", marginLeft: "calc(50% - 10rem)"}}>
                          
             </div>
-<p class="text-center">Cancer Research UK (CRUK) Career Development Fellowship</p>
+<p class="text-center"><strong>Cancer Research UK (CRUK) Career Development Fellowship</strong></p>
             <div className="width-100 flex-column text-center">
                 <p>“Targeting Post-transcriptional Regulators to Eradicate Leukemic Stem Cells in Acute Myeloid Leukemia”. 1,258,197GBP. 2022-2028</p>
-                <p><strong>"American Society of Hematology Scholar Award". $100,000. 2021-2023</strong></p>
+                <p><strong>American Society of Hematology Scholar Award. </strong></p>
+                <p>“Uncovering differential translational program in AML”. $100,000. 2021-2023</p>
             </div>
             </div>
         </Layout>
