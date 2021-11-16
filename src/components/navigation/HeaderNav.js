2b9db1f26838c40952a95c1f6dd5e967a7cd8452
Gareth Fentimen <garethfentimen@gmail.com>
2021-11-16 17:25:58 -0500

    mobile menu

--- src/components/navigation/HeaderNav.js
+++ src/components/navigation/HeaderNav.js
@@ -4,47 +4,61 @@ import logo from '../../images/CRUK_BARTS_C_Neg_White.png';
 import './HeaderNav.scss';
 
 export default function HeaderNav() {
+
+
+
+    function mobileMenu() {
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu");
+        hamburger && hamburger.classList.toggle("active");
+        navMenu && navMenu.classList.toggle("active");
+    }
     return <>
         <div className="pre-header"></div>
         <header>
-            <Link to={`/`} className="logo">
+            <Link to={`/`} className="logo nav-logo">
                 <span>DiuNguyen Lab</span>
             </Link>
-            <nav className="">
-                <ul>
-                    <li>
+            <nav className="navbar">
+                <ul className="nav-menu" onClick={mobileMenu}>
+                    <li className="nav-item">
                         <Link to={`/`}>
                             Home
                         </Link>
                     </li>
-                    <li>
+                    <li className="nav-item">
                         <Link to={`/research`}>
                             Research
                         </Link>
                     </li>
-                    <li>
+                    <li className="nav-item">
                         <Link to={`/people`}>
                             <div className="flex">
                                 People
                             </div>
                         </Link>
                     </li>
-                    <li>
+                    <li className="nav-item">
                         <Link to={`/funding`}>
                             Funding
                         </Link>
                     </li>
-                    <li>
+                    <li className="nav-item">
                         <Link to={`/publications`}>
                             Publications
                         </Link>
                     </li>
-                    <li>
+                    <li className="nav-item">
                         <Link to={`/contact`}>
                             Contact
                         </Link>
                     </li>
                 </ul>
+                <div className="hamburger" onClick={mobileMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
             </nav>
         </header>
     </>
