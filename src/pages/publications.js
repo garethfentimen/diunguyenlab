c62449162ce42755856d7fa98348f7520526b2cd
DiuTTNguyen <thanhdiu.nguyen@gmail.com>
2021-11-28 13:43:43 -0500

    Update publications.js

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -15,16 +15,16 @@ export default function research() {
                     </h3>
                     <ul>
                         <li>
-                            <div>Ly P Vu Florisela Herrejon Chavez, Paolo Cifani, Alli Pine, Karen L Chu, Ersilia Barin, Alexandra Schurer, Hanzhi Luo, <strong>Diu T.T. Nguyen</strong>, Alex Kentsis, Christina Leslie, Michael G Kharas. 2021. <i>RNA Binding Protein Syncrip Is Required for the Low-Output HSC By Sustaining Proteome Quality</i>. Blood. 138 (Supplement 1): 296.</div>
+                            <div>Florisela Herrejon Chavez, Paolo Cifani, Alli Pine, Karen L Chu, Ersilia Barin, Alexandra Schurer, Hanzhi Luo, <strong>Diu T.T. Nguyen</strong>, Alex Kentsis, Christina Leslie, Michael G Kharas, Ly P Vu. 2021. <i>RNA Binding Protein Syncrip Is Required for the Low-Output HSC By Sustaining Proteome Quality</i>. Blood. 138 (Supplement 1): 296.</div>
                         </li>
                         <li>
-                            <div>Coraline Mlynarczyk, Matthew Teater, Juhee Pae, Theinmozhi Arulraj, Christopher R Chin, Ling Wang, Jonatan Ersching, Ekaterina Kots, Amy Chadburn, Ersilia Barin, Hui Xian Poh, Hannah M Isles, Chen Zhengming, Hao Shen, Arnab Bandyopadhyay, Kenneth B Hoehn, <strong>Diu T.T. Nguyen</strong>, Ashley S Doane, Huimin Geng, Muhammad H Rehman, Jonah Melnick, Wyatt Morgan, Olivier Elemento, Michael G Kharas, David W. Scott, Samie R Jaffrey, Michael Meyer-Hermann, George Khelashvili, Gabriel Victora, Ari Melnick. 2021. BTG1 Mutation Promotes Aggressive Lymphoma Development By Lowering the Threshold to MYC Activation and Generating "Super-Competitor" B Cells. Blood (Supplement 1): 359.</div>
+                            <div>Coraline Mlynarczyk, Matthew Teater, Juhee Pae, Theinmozhi Arulraj, Christopher R Chin, Ling Wang, Jonatan Ersching, Ekaterina Kots, Amy Chadburn, Ersilia Barin, Hui Xian Poh, Hannah M Isles, Chen Zhengming, Hao Shen, Arnab Bandyopadhyay, Kenneth B Hoehn, <strong>Diu T.T. Nguyen</strong>, Ashley S Doane, Huimin Geng, Muhammad H Rehman, Jonah Melnick, Wyatt Morgan, Olivier Elemento, Michael G Kharas, David W. Scott, Samie R Jaffrey, Michael Meyer-Hermann, George Khelashvili, Gabriel Victora, Ari Melnick. (2021). <i>BTG1 Mutation Promotes Aggressive Lymphoma Development By Lowering the Threshold to MYC Activation and Generating "Super-Competitor" B Cells</i>. Blood (Supplement 1): 359.</div>
                         </li>
                         <li>
-                            <div>Camila Prieto*, <strong>Diu TT Nguyen</strong>*, Zhaoqi Liu, Justin Wheat, Alexendar Perez, Saroj Gourkanti, Timothy Chou, Ersilia Barin, Anthony Velleca, Thomas Rohwetter, Arthur Chow, James Taggart, Angela M Savino, Katherina Hoskova, Meera Dhodapkar, Alexandra Schurer, Trevor S Barlowe, Ly P Vu, Christina Leslie, Ulrich Steidl, Raul Rabadan, Michael G Kharas. 2021. Transcriptional control of CBX5 by the RNA binding proteins RBMX and RBMXL1 maintain chromatin state in myeloid leukemia. Nature Cancer 2, 741-757. * Equal contribution.</div>
+                            <div>Camila Prieto*, <strong>Diu TT Nguyen</strong>*, Zhaoqi Liu, Justin Wheat, Alexendar Perez, Saroj Gourkanti, Timothy Chou, Ersilia Barin, Anthony Velleca, Thomas Rohwetter, Arthur Chow, James Taggart, Angela M Savino, Katherina Hoskova, Meera Dhodapkar, Alexandra Schurer, Trevor S Barlowe, Ly P Vu, Christina Leslie, Ulrich Steidl, Raul Rabadan, Michael G Kharas. (2021). <i>Transcriptional control of CBX5 by the RNA binding proteins RBMX and RBMXL1 maintain chromatin state in myeloid leukemia</i>. Nature Cancer 2, 741-757. * Equal contribution.</div>
                         </li>
                         <li>
-                            <div>Yuanming Cheng, Wei Xie, Brian F Pickering, Karen L Chu, Angela M Savino, Xuejing Yang, Hanzhi Luo, <strong>Diu TT Nguyen</strong>, Ersilia Barin, Anthony Velleca, Thomas Rohwetter, Dinshaw J Patel, Samie R Jaffrey and Michael G Kharas. (2021). YTHDC1-m6A-mRNA mediated liquid-liquid phase condensates are required for myeloid leukemia. Cancer Cell 38(7), 958-972.</div>
+                            <div>Yuanming Cheng, Wei Xie, Brian F Pickering, Karen L Chu, Angela M Savino, Xuejing Yang, Hanzhi Luo, <strong>Diu TT Nguyen</strong>, Ersilia Barin, Anthony Velleca, Thomas Rohwetter, Dinshaw J Patel, Samie R Jaffrey and Michael G Kharas. (2021). <i>YTHDC1-m6A-mRNA mediated liquid-liquid phase condensates are required for myeloid leukemia</i>. Cancer Cell 38(7), 958-972.</div>
                         </li>
                     </ul>
                     <h3 style={{ fontFamily: 'Open-Sans', color: '#454f58' }}>
@@ -32,7 +32,7 @@ export default function research() {
                     </h3>
                     <ul>
                         <li>
-                            <div><strong>Diu TT Nguyen</strong>, Yuheng Lu, Karen L Chu, Xuejing Yang, Sun-Mi Park, Zi-Ning Choo, Christopher R Chin, Camila Prieto, Alexandra Schurer, Ersilia Barin, Angela M Savino, Saroj Gourkanti, Payal Patel, Ly P Vu, Christina Leslie, Michael G Kharas. (2020). HyperTRIBE uncovers increased MUSASHI-2 RNA binding activity and differential regulation in leukemia stem cells. Nature Communications 11 (2026).</div>
+                            <div><strong>Diu TT Nguyen</strong>, Yuheng Lu, Karen L Chu, Xuejing Yang, Sun-Mi Park, Zi-Ning Choo, Christopher R Chin, Camila Prieto, Alexandra Schurer, Ersilia Barin, Angela M Savino, Saroj Gourkanti, Payal Patel, Ly P Vu, Christina Leslie, Michael G Kharas. (2020). <i>HyperTRIBE uncovers increased MUSASHI-2 RNA binding activity and differential regulation in leukemia stem cells</i>. Nature Communications 11 (2026).</div>
                         </li>
                     </ul>
                     <h3 style={{ fontFamily: 'Open-Sans', color: '#454f58' }}>
@@ -40,7 +40,7 @@ export default function research() {
                     </h3>
                     <ul>
                         <li>
-                            <div>Yuanming Cheng*, Hanzhi Luo*, Franco Izzo*, Brian F Pickering, <strong>Diu Nguyen</strong>, Robert Myers, Alexandra Schurer, Saroj Gourkanti, Jens C Brüning, Ly P Vu, Samie R Jaffrey, Dan A Landau, Michael G Kharas (2019). RNA methylation maintains hematopoietic stem cell identity and symmetric commitment. Cell Reports 28(7), 1703-1716. * Equal contribution.</div>
+                            <div>Yuanming Cheng*, Hanzhi Luo*, Franco Izzo*, Brian F Pickering, <strong>Diu Nguyen</strong>, Robert Myers, Alexandra Schurer, Saroj Gourkanti, Jens C Brüning, Ly P Vu, Samie R Jaffrey, Dan A Landau, Michael G Kharas (2019). <i>RNA methylation maintains hematopoietic stem cell identity and symmetric commitment</i>. Cell Reports 28(7), 1703-1716. * Equal contribution.</div>
                         </li>
                     </ul>
                     <h3 style={{ fontFamily: 'Open-Sans', color: '#454f58' }}>
@@ -48,10 +48,10 @@ export default function research() {
                     </h3>
                     <ul>
                         <li>
-                            <div>Ly P Vu*, Brian F Pickering*, Yuanming Cheng*, Sara Zaccara, <strong>Diu Nguyen</strong>, Gerard Minuesa, Timothy Chou, Arthur Chow, Yogesh Saletore, Matthew MacKay, Jessica Schulman, Christopher Famulare, Minal Patel, Virginia M Klimek, Francine E Garrett-Bakelman, Ari Melnick, Martin Carroll, Christopher E Mason, Samie R Jaffrey, Michael G Kharas. (2017). The N6 methyladenosine (m6A)-forming enzyme METTL3 controls myeloid differentiation of normal and leukemia cells. Nature Medicine 23(11), 1369 (Featured on the cover). * Equal contribution.</div>
+                            <div>Ly P Vu*, Brian F Pickering*, Yuanming Cheng*, Sara Zaccara, <strong>Diu Nguyen</strong>, Gerard Minuesa, Timothy Chou, Arthur Chow, Yogesh Saletore, Matthew MacKay, Jessica Schulman, Christopher Famulare, Minal Patel, Virginia M Klimek, Francine E Garrett-Bakelman, Ari Melnick, Martin Carroll, Christopher E Mason, Samie R Jaffrey, Michael G Kharas. (2017). <i>The N6 methyladenosine (m6A)-forming enzyme METTL3 controls myeloid differentiation of normal and leukemia cells</i>. Nature Medicine 23(11), 1369 (Featured on the cover). * Equal contribution.</div>
                         </li>
                         <li>
-                            <div><strong>Diu TT Nguyen</strong>, Hsiao Phin J Voon, Barbara Xella, Caroline Scott, David Clynes, Christian Babbs, Helena Ayyub, Jon Kerry, Jacqueline A Sharpe, Jackie A Sloane‐Stanley, Sue Butler, Chris A Fisher, Nicki E Gray, Thomas Jenuwein, Douglas R Higgs, Richard J Gibbons. (2017).  The chromatin remodelling factor ATRX suppresses R-loops in transcribed telomeric repeats. EMBO Report 18(6), 914-928. (Featured on the cover).</div>
+                            <div><strong>Diu TT Nguyen</strong>, Hsiao Phin J Voon, Barbara Xella, Caroline Scott, David Clynes, Christian Babbs, Helena Ayyub, Jon Kerry, Jacqueline A Sharpe, Jackie A Sloane‐Stanley, Sue Butler, Chris A Fisher, Nicki E Gray, Thomas Jenuwein, Douglas R Higgs, Richard J Gibbons. (2017).  <i>The chromatin remodelling factor ATRX suppresses R-loops in transcribed telomeric repeats</i>. EMBO Report 18(6), 914-928. (Featured on the cover).</div>
                         </li>
                     </ul>
                     <h3 style={{ fontFamily: 'Open-Sans', color: '#454f58' }}>
@@ -59,10 +59,10 @@ export default function research() {
                     </h3>
                     <ul>
                         <li>
-                            <div>Marina Bousquet, <strong>Diu Nguyen</strong>, Cynthia Chen, Lauren Shields, Harvey F Lodish. (2012). miR-125b transforms myeloid cell lines by repressing multiple mRNA targets. Hematologica 97(11), 1713-1721.</div>
+                            <div>Marina Bousquet, <strong>Diu Nguyen</strong>, Cynthia Chen, Lauren Shields, Harvey F Lodish. (2012). <i>miR-125b transforms myeloid cell lines by repressing multiple mRNA targets</i>. Hematologica 97(11), 1713-1721.</div>
                         </li>
                         <li>
-                            <div>Huyen T La, Thu T Dang, Quyen H Dao, <strong>Diu TT Nguyen</strong>, Huan Q Le (2008). Expression of recombinant antibodies specific to CD33 antigen. Journal of Medicine (Vietnamese) (2), 47-52</div>
+                            <div>Huyen T La, Thu T Dang, Quyen H Dao, <strong>Diu TT Nguyen</strong>, Huan Q Le (2008). <i>Expression of recombinant antibodies specific to CD33 antigen</i>. Journal of Medicine (Vietnamese) (2), 47-52</div>
                         </li>
                     </ul>
                 </div>
@@ -70,10 +70,10 @@ export default function research() {
                     Other Publications
                 </h3>
                 <ul>
-                    <li><strong>Nguyen DTT</strong>, Prieto C, Kharas MG. (2021). New therapeutic targets: RNA binding proteins RBMX/L1 control nascent transcription in leukemia. Behind the paper. Cancer community. </li>
-                    <li><strong>Nguyen D</strong>, Prieto C, Liu Z, Wheat J, Perez A, Gourkanti, Chou T, Barin E, Chow Arthur, Taggart James, Hoskova K, Dhodapkar M, Schurer A, Barlowe T, Vu L, Leslie C, Steidl U, Rabadan R, Kharas M. (2020). Transcriptional Control of HP1a By the RNA Binding Proteins Rbmx/L1 Maintain Chromatin State in Myeloid Leukemia. Blood. 136. 15-15. 10.1182/blood-2020-142229.</li>
+                    <li><strong>Nguyen DTT</strong>, Prieto C, Kharas MG. (2021). <i>New therapeutic targets: RNA binding proteins RBMX/L1 control nascent transcription in leukemia</i>. Behind the paper. Cancer community. </li>
+                    <li><strong>Nguyen D</strong>, Prieto C, Liu Z, Wheat J, Perez A, Gourkanti, Chou T, Barin E, Chow Arthur, Taggart James, Hoskova K, Dhodapkar M, Schurer A, Barlowe T, Vu L, Leslie C, Steidl U, Rabadan R, Kharas M. (2020). <i>Transcriptional Control of HP1a By the RNA Binding Proteins Rbmx/L1 Maintain Chromatin State in Myeloid Leukemia</i>. Blood. 136. 15-15. 10.1182/blood-2020-142229.</li>
                     <li>Nguyen H., Nguyen L., <strong>Nguyen D.</strong>, Trinh L., Le M., Nguyen T., Tran T., Nguyen N., Le H. Molecular Cell Bilogy (7th edition by Lodish H., et al.) Vietnamese Translation book, Chapter 8, volume 2. (July 2014); Chapter 24, volume 5 (Jan 2019). Tre Publishing House.</li>
-                    <li><strong>Diu TT Nguyen</strong>, Huyen T La, Trong N Pham, Huan Q Le (2007). Cloning and sequencing Her2/new specific to breast cancer. Proceeding, the 7th National Conference on Life Sciences, Aug 2007.</li>
+                    <li><strong>Diu TT Nguyen</strong>, Huyen T La, Trong N Pham, Huan Q Le (2007). <i>Cloning and sequencing Her2/new specific to breast cancer</i>. Proceeding, the 7th National Conference on Life Sciences, Aug 2007.</li>
                 </ul>
                 
                 <div className="text-center pad-md">
