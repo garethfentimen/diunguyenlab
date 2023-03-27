9782596c40e52d4ccafe8007b1cb45724e8fd72f
DiuTTNguyen <thanhdiu.nguyen@gmail.com>
2023-03-27 23:37:24 +0100

    Update news.js

--- src/pages/news.js
+++ src/pages/news.js
@@ -3,7 +3,8 @@ import campus_winter from '../images/Campus_winter.jpg';
 
 import React from 'react'
 import Layout from '../components/layout/layout'
-import Seo from "../components/seo/seo"
+import Seo from '../components/seo/seo'
+import './news.scss'
 
 export default function News() {
     return (
@@ -17,7 +18,7 @@ binding proteins in B-cell lymphoma. Recruitment of a PhD student position in pr
 project starts in October 2023 or January 2024. Please get in touch for more information.</p>
 
 <h4>Our first Christmas lab lunch</h4>
-  <div className="flex">
+  <div className="flex lab-images">
 <img style={{ width: "50%" }} src={lab_lunch} alt="lab lunch" />
 <img style={{ width: "50%" }} src={campus_winter} alt="campus winter" />
      </div>
