d87b581adf87f778b392c999b3a24d8759dca3ae
DiuTTNguyen <thanhdiu.nguyen@gmail.com>
2023-11-10 23:29:49 +0000

    Update news.js

--- src/pages/news.js
+++ src/pages/news.js
@@ -12,80 +12,79 @@ export default function News() {
     return (
      <Layout>
         <Seo title="News" />
-          <div className="flex-column">
-              <div className="flex-column">
-                <h2>November 2023</h2>
-                <p>Congratulations to Khadidja for landing a great position at the Francis Crick institute and Nilo for becoming an NHS medical scientist at Bedfordshire hospital!</p> 
-              </div>
-            
-              <div className="flex-column">
-              <h2>September 2023</h2>
-              <p>We celebrated the successful lab project completion of our three amazing students, Wisdom, Nilo and Siddhi. Well done!</p>
-              </div>
-    
-              <h4>Pizza on the green</h4>
-              <div className="flex lab-images">
-                <img style={{ width: "30%" }} src={summer2023_1} alt="Pizza party" />
-                <img style={{ width: "30%" }} src={summer2023_2} alt="Pizza party star" />
-              </div>
-          </div>
+        <div className="flex-column">
           
           <div className="flex-column">
-            <h2>March 2023</h2>
-
-            <p>Our lab won a Barts Charity research grant, which will allow us to work on the role of RNA-
-  binding proteins in B-cell lymphoma. Recruitment of a PhD student position in progress! The
-  project starts in October 2023 or January 2024. Please get in touch for more information.</p>
-
-            <h4>Our first Christmas lab lunch</h4>
+            <h2>November 2023</h2>
+            <p>Congratulations to Khadidja for landing a great position at the Francis Crick institute and Nilo for becoming an NHS medical scientist at Bedfordshire hospital!</p> 
+          </div>
         
-        <div className="flex lab-images">
-          <img style={{ width: "30%" }} src={lab_lunch} alt="lab lunch" />
-          <img style={{ width: "50%" }} src={campus_winter} alt="campus winter" />
-        </div>
+          <div className="flex-column">
+            <h2>September 2023</h2>
+            <p>We celebrated the successful lab project completion of our three amazing students, Wisdom, Nilo and Siddhi. Well done!</p>
+          </div>
 
-        <div className="flex-column">
-          <h2>December 2022</h2>
-          <p>We welcomed two members of the DiuNguyen lab: Khadidja Habel joined the lab as a research
-          technician under ASH funding and James Boncan, PhD as a postdoctoral research assistant
-          under our CRUK CDF grant.</p>
-        </div>
+          <h4>Pizza on the green</h4>
+          <div className="flex lab-images">
+            <img style={{ width: "30%" }} src={summer2023_1} alt="Pizza party" />
+            <img style={{ width: "30%" }} src={summer2023_2} alt="Pizza party star" />
+          </div>
 
-        <div className="flex-column">
-          <h2>October 2022</h2>
-          <p>Wisdom Igiogbe, undergraduate student at Kent University, started his one-year
-          placement as part of his sandwich programme at Queen Mary.</p>
-        </div>
+          <h2>March 2023</h2>
 
-        <div className="flex-column">
-          <h2>September 2022</h2>
-          <p>Our first official member Kim-Phuong To, PhD joined the lab. She arrived in London on her
-          birthday, how special!</p>
-        </div>
+          <p>Our lab won a Barts Charity research grant, which will allow us to work on the role of RNA-
+binding proteins in B-cell lymphoma. Recruitment of a PhD student position in progress! The
+project starts in October 2023 or January 2024. Please get in touch for more information.</p>
 
-        <div className="flex-column">
-          <h2>August 2022</h2>
-          <p>Our lab was awarded a CRUK City of London (CoL) Development Fund which started a brand-
-          new exciting direction of research in the lab – investigating the role of translational regulation
-          in cancer progression.</p>
-        </div>
+          <h4>Our first Christmas lab lunch</h4>
+    
+          <div className="flex lab-images">
+              <img style={{ width: "30%" }} src={lab_lunch} alt="lab lunch" />
+              <img style={{ width: "50%" }} src={campus_winter} alt="campus winter" />
+          </div>
 
-        <div className="flex-column">
-        <h2>July 2022</h2>
-        <p>Rana Elnady and Khadidja Habel joined the lab as MSc placement students.</p>
-        </div>
+          <div className="flex-column">
+              <h2>December 2022</h2>
+              <p>We welcomed two members of the DiuNguyen lab: Khadidja Habel joined the lab as a research
+              technician under ASH funding and James Boncan, PhD as a postdoctoral research assistant
+              under our CRUK CDF grant.</p>
+          </div>
 
-        <div className="flex-column">
-        <h2>May 2022</h2>
-        <p>Bench and equipment are ready for experiments!</p>
-        </div>
+          <div className="flex-column">
+              <h2>October 2022</h2>
+              <p>Wisdom Igiogbe, undergraduate student at Kent University, started his one-year
+              placement as part of his sandwich programme at Queen Mary.</p>
+          </div>
 
-        <div className="flex-column">
-          <h2>1st April 2022</h2>
-          <p>The DiuNguyen laboratory is officially opened at the Centre for Haemato-Oncology, Barts
-          Cancer Institute, QMUL.</p>
-        </div>
-      </div>
+          <div className="flex-column">
+              <h2>September 2022</h2>
+              <p>Our first official member Kim-Phuong To, PhD joined the lab. She arrived in London on her
+              birthday, how special!</p>
+          </div>
+    
+          <div className="flex-column">
+              <h2>August 2022</h2>
+              <p>Our lab was awarded a CRUK City of London (CoL) Development Fund which started a brand-
+              new exciting direction of research in the lab – investigating the role of translational regulation
+              in cancer progression.</p>
+          </div>
+    
+          <div className="flex-column">
+            <h2>July 2022</h2>
+            <p>Rana Elnady and Khadidja Habel joined the lab as MSc placement students.</p>
+          </div>
+    
+          <div className="flex-column">
+            <h2>May 2022</h2>
+            <p>Bench and equipment are ready for experiments!</p>
+          </div>
+    
+          <div className="flex-column">
+              <h2>1st April 2022</h2>
+              <p>The DiuNguyen laboratory is officially opened at the Centre for Haemato-Oncology, Barts
+              Cancer Institute, QMUL.</p>
+          </div>
+       </div>
     </Layout>
     )
 }
