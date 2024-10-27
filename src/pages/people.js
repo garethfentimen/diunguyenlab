b511d6f2c492301c505d9c482062b104bba14453
DiuTTNguyen <thanhdiu.nguyen@gmail.com>
2024-10-27 21:41:07 +0000

    Update people.js

--- src/pages/people.js
+++ src/pages/people.js
@@ -17,40 +17,42 @@ export default function People() {
                 <div className="flex-column text-center">
                     <h1>Meet the team</h1>
                 </div>
-                <div className='flex'>
-                    <div>
-                        <div className="flex pi">
-                            <div className="flex-column">
-                                <h3 style={{color: 'gray'}}>Principle Investigator:</h3>
+                <div class="flex-column">
+                    <div className='flex'>
+                        <div>
+                            <div className="flex pi">
+                                <div className="flex-column">
+                                    <h3 style={{color: 'gray'}}>Principle Investigator:</h3>
+                                </div>
+                                <div>
+                                    <h3 className="marg-left-xs">Diu T.T. Nguyen, PhD</h3>  
+                                </div>
                             </div>
-                            <div>
-                                <h3 className="marg-left-xs">Diu T.T. Nguyen, PhD</h3>  
+                            
+                            <div className="diu-desc">
+                                <p>
+                                Diu obtained her MSc in Biomolecular Sciences from Vrije Universiteit Amsterdam and the Whitehead Institute for Biomedical Research, MIT. 
+                                She was then awarded a Marie Curie fellowship to complete her PhD (DPhil) from the University of Oxford, UK. 
+                                Diu then moved to the Memorial Sloan-Kettering Cancer Center in New York, USA for her postdoc, investigating the role of post-transcriptional regulation in normal and malignant cancer stem cells. 
+                                She pioneered the use of RNA-editing based approaches to identify RNA targets of RNA-Binding Proteins (RBPs) in mammalian stem cells, which has provided new insights into RBP function in rare cells and opened a new avenue for stem cell research. 
+                                She also discovered the oncogenic role of a number of RBPs in blood cancer. 
+                                Diu is a recipient of the American Society of Hematology (ASH) Scholar Fellowship that supports her transition to independence. 
+                                In 2022, Diu started her own group at the Centre for Haemato-Oncology, Barts Cancer Institute, with the support of a Career Development Fellowship from Cancer Research UK.
+                                </p>
+                                <p>For more information about Diu,please find her on <a href="https://twitter.com/diunguyen_vn_uk?s=20" target="_blank" rel="noopener noreferrer">Twitter</a>,<span> </span>
+                                    <a href="https://orcid.org/0000-0001-8328-0009" target="_blank" rel="noopener noreferrer">Orchid</a>,<span> </span>
+                                    <a href="https://www.researchgate.net/profile/Diu-Nguyen" target="_blank" rel="noopener noreferrer">Research Gate</a>,<span> </span>
+                                    <a href="https://scholar.google.com/citations?user=yZb8MqEAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">Google Scholar</a>.</p> 
                             </div>
                         </div>
-                        
-                        <div className="diu-desc">
-                            <p>
-                            Diu obtained her MSc in Biomolecular Sciences from Vrije Universiteit Amsterdam and the Whitehead Institute for Biomedical Research, MIT. 
-                            She was then awarded a Marie Curie fellowship to complete her PhD (DPhil) from the University of Oxford, UK. 
-                            Diu then moved to the Memorial Sloan-Kettering Cancer Center in New York, USA for her postdoc, investigating the role of post-transcriptional regulation in normal and malignant cancer stem cells. 
-                            She pioneered the use of RNA-editing based approaches to identify RNA targets of RNA-Binding Proteins (RBPs) in mammalian stem cells, which has provided new insights into RBP function in rare cells and opened a new avenue for stem cell research. 
-                            She also discovered the oncogenic role of a number of RBPs in blood cancer. 
-                            Diu is a recipient of the American Society of Hematology (ASH) Scholar Fellowship that supports her transition to independence. 
-                            In 2022, Diu started her own group at the Centre for Haemato-Oncology, Barts Cancer Institute, with the support of a Career Development Fellowship from Cancer Research UK.
-                            </p>
-                            <p>For more information about Diu,please find her on <a href="https://twitter.com/diunguyen_vn_uk?s=20" target="_blank" rel="noopener noreferrer">Twitter</a>,<span> </span>
-                                <a href="https://orcid.org/0000-0001-8328-0009" target="_blank" rel="noopener noreferrer">Orchid</a>,<span> </span>
-                                <a href="https://www.researchgate.net/profile/Diu-Nguyen" target="_blank" rel="noopener noreferrer">Research Gate</a>,<span> </span>
-                                <a href="https://scholar.google.com/citations?user=yZb8MqEAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">Google Scholar</a>.</p> 
-                        </div>
-                    </div>
-                    <div className="align-right image-right">
-                        <img className="pad-lg" style={{height: "35%"}} src={diu} alt="Dr Diu Nguyen" />
-                        <div className="image-text marg-left-lg">
-                            {/* <p>Group Leader</p>
-                            <p>Lecturer</p> */}
-                            <p>CRUK Career Development Fellow</p>
-                            <p>ASH Scholar Fellow</p>
+                        <div className="align-right image-right">
+                            <img className="pad-lg" style={{height: "35%"}} src={diu} alt="Dr Diu Nguyen" />
+                            <div className="image-text marg-left-lg">
+                                {/* <p>Group Leader</p>
+                                <p>Lecturer</p> */}
+                                <p>CRUK Career Development Fellow</p>
+                                <p>ASH Scholar Fellow</p>
+                            </div>
                         </div>
                     </div>
                 </div>
