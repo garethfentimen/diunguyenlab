fd81e03eede82fce07facd138327a9c1f65fb6f6
DiuTTNguyen <thanhdiu.nguyen@gmail.com>
2025-03-21 14:00:21 +0000

    Update publications.js

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -6,10 +6,25 @@ export default function research() {
         <Layout>
             <div className="publications flex-column width-70">
                 <h3 style={{ fontFamily: 'Exo2', color: 'gray' }}>
-                    Peer-reviewed Publications
+                    Selected Peer-reviewed Publications
                 </h3>
 
                 <div className="marg-left-md line-height-md">
+                    <h3 style={{ fontFamily: 'Open-Sans', color: '#454f58' }}>
+                    2024
+                    </h3>
+                    <ul>
+                        <li>
+                            <div>Xie X., Chavez F.H., Baali I., Chu E. L., Evans C. M., Yang X., Wei W., Luo H., Park S.M., Chang K., Pierson A.J., Han G., Batchelor E., Morris Q. D., Vu L.P., <strong> Nguyen D.T.T.* </strong>, Kharas M.G.* 2024. <i>Uncovering the HOXA9 translational regulatory complex that promotes AML leukemic stem cells.</i> (*co-corresponding author). Blood (144, Supplement 1, page 4106). </div>
+                        </li>
+                        <li>
+                            <div>Wakiro I., Yue H., Yang X., Xie X., Chang K., Pierson A.J., Han G., Batchelor E., Cheng Y., <strong>Nguyen D.T.T.</strong>, Liu Z., Kharas M.G. 2024. <i>RBMX regulates YTHDC1 nuclear condensates to promote nascent transcription in acute myeloid leukemia.</i> Blood (144, Supplement 1, page 627).</div>                  </li>
+                        </li>
+                        <li>
+                            <div>Wagstaff M.,<strong> Nguyen D.T.T </strong>, Blair A., Towler B.P., Newbury S.F., Morgan R.G., (2024). <i>A β-catenin: MSI2 axis regulates the expression of LEF1 and subsequent human haematopoietic stem/progenitor cell proliferation. </i> Biorxiv. </div>
+                        </li>
+                    </ul>
+                    
                     <h3 style={{ fontFamily: 'Open-Sans', color: '#454f58' }}>
                     2023
                     </h3>
