d866e6ba5b8b415096c68db0410aa8058bfd7bf1
garethfentimen <garethfentimen@gmail.com>
2025-07-08 23:27:53 +0100

    all lab members to have full

--- src/pages/people.js
+++ src/pages/people.js
@@ -16,76 +16,91 @@ export default function People() {
                 <div className="flex-column text-center">
                     <h1>Meet the team</h1>
                 </div>
-                <div className="flex-column grid-bg pad-btm-lg marg-btm-xl">
-                    <div className='flex diu-text-container'>
-                        <div style={{'max-width': '80%'}}>
-                            <div className="flex pi">
-                                <div className="flex-column">
-                                    <h3 style={{color: 'gray'}}>Principle Investigator:</h3>
-                                </div>
-                                <div>
-                                    <h3 className="marg-left-xs">Diu T.T. Nguyen, PhD</h3>  
-                                </div>
+
+                <div className="people-grid">
+                    <div className="flex grid-bg">
+                        <div class="marg-left-sm">
+                            <div class="text-center">
+                                <h3>Diu T.T. Nguyen, PhD</h3>
+                                <h4>Principle Investigator</h4>
                             </div>
-                            
-                            <div className="diu-desc">
-                                <p>
-                                Diu obtained her MSc in Biomolecular Sciences from Vrije Universiteit Amsterdam and the Whitehead Institute for Biomedical Research, MIT. 
-                                She was then awarded a Marie Curie fellowship to complete her PhD (DPhil) from the University of Oxford, UK. 
-                                Diu then moved to the Memorial Sloan-Kettering Cancer Center in New York, USA for her postdoc, investigating the role of post-transcriptional regulation in normal and malignant cancer stem cells. 
-                                She pioneered the use of RNA-editing based approaches to identify RNA targets of RNA-Binding Proteins (RBPs) in mammalian stem cells, which has provided new insights into RBP function in rare cells and opened a new avenue for stem cell research. 
-                                She also discovered the oncogenic role of a number of RBPs in blood cancer. 
-                                Diu is a recipient of the American Society of Hematology (ASH) Scholar Fellowship that supports her transition to independence. 
-                                In 2022, Diu started her own group at the Centre for Haemato-Oncology, Barts Cancer Institute, with the support of a Career Development Fellowship from Cancer Research UK.
-                                </p>
-                                <p>For more information about Diu,please find her on <a href="https://twitter.com/diunguyen_vn_uk?s=20" target="_blank" rel="noopener noreferrer">Twitter</a>,<span> </span>
-                                    <a href="https://orcid.org/0000-0001-8328-0009" target="_blank" rel="noopener noreferrer">Orchid</a>,<span> </span>
-                                    <a href="https://www.researchgate.net/profile/Diu-Nguyen" target="_blank" rel="noopener noreferrer">Research Gate</a>,<span> </span>
-                                    <a href="https://scholar.google.com/citations?user=yZb8MqEAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">Google Scholar</a>.</p> 
+                            <div className="member-desc">
+                                    <p>
+                                    Diu obtained her MSc in Biomolecular Sciences from Vrije Universiteit Amsterdam and the Whitehead Institute for Biomedical Research, MIT. 
+                                    She was then awarded a Marie Curie fellowship to complete her PhD (DPhil) from the University of Oxford, UK. 
+                                    Diu then moved to the Memorial Sloan-Kettering Cancer Center in New York, USA for her postdoc, investigating the role of post-transcriptional regulation in normal and malignant cancer stem cells. 
+                                    </p>
+                                    <p>
+                                        She pioneered the use of RNA-editing based approaches to identify RNA targets of RNA-Binding Proteins (RBPs) in mammalian stem cells, which has provided new insights into RBP function in rare cells and opened a new avenue for stem cell research. 
+                                    She also discovered the oncogenic role of a number of RBPs in blood cancer. 
+                                    Diu is a recipient of the American Society of Hematology (ASH) Scholar Fellowship that supports her transition to independence. 
+                                    In 2022, Diu started her own group at the Centre for Haemato-Oncology, Barts Cancer Institute, with the support of a Career Development Fellowship from Cancer Research UK.
+                                    </p>
+                                    <p>For more information about Diu,please find her on <a href="https://twitter.com/diunguyen_vn_uk?s=20" target="_blank" rel="noopener noreferrer">Twitter</a>,<span> </span>
+                                        <a href="https://orcid.org/0000-0001-8328-0009" target="_blank" rel="noopener noreferrer">Orcid</a>,<span> </span>
+                                        <a href="https://www.researchgate.net/profile/Diu-Nguyen" target="_blank" rel="noopener noreferrer">Research Gate</a>,<span> </span>
+                                        <a href="https://scholar.google.com/citations?user=yZb8MqEAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">Google Scholar</a>.
+                                    </p> 
                             </div>
                         </div>
                         <div className="align-right image-right">
-                            <img className="pad-lg" style={{width: "100%"}} src={diu} alt="Dr Diu Nguyen" />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="people-grid">
-                    <div className="text-center grid-bg">
-                        <h3 className="no-margin">Kim-Phuong To</h3>
-                        <h3 style={{color: 'gray'}} className="no-margin">Postdoctoral fellow</h3>
-                        <div className="pad-md">
-                            <img style={{ width: "40%" }} className="" src={phuong} alt="Kim-Phuong To" />
+                            <img className="pad-md people-image" src={diu} alt="Dr Diu Nguyen" />
                         </div>
                     </div>
 
-                    <div className="text-center grid-bg">
-                        <h3 className="no-margin">Bertina Dragunaite</h3>
-                        <h3 style={{color: 'gray'}} className="no-margin">PhD student</h3>
-                        <div className="pad-md">
-                            <img style={{ width: "40%" }} className="" src={bertina} alt="Bertina Dragnunaite" />
+                    <div className="flex grid-bg">
+                        <div class="marg-left-sm">
+                            <div class="text-center">
+                                <h3>Kim-Phuong To</h3>
+                                <h4>Postdoctoral fellow</h4>
+                            </div>
+                            <div></div>
+                        </div>
+                        <div className="align-right image-right">
+                            <img className="pad-md people-image" src={phuong} alt="Kim-Phuong To" />
                         </div>
                     </div>
 
-                    <div className="text-center grid-bg">
-                        <h3 className="no-margin">Lauren Harrison-Oakes</h3>
-                        <h3 style={{color: 'gray'}} className="no-margin">PhD student</h3>
-                        <div className="pad-md">
-                            <img style={{ width: "40%" }} className="" src={lauren} alt="Lauren Harrison-Oakes" />
+                    <div className="flex grid-bg">
+                        <div class="marg-left-sm">
+                            <div class="text-center">
+                                <h3>Bertina Dragunaite</h3>
+                                <h4>PhD student</h4>
+                            </div>
+                        </div>
+                        <div className="align-right image-right">
+                            <img src={bertina} className="pad-md people-image" alt="Bertina Dragnunaite" />
                         </div>
                     </div>
-                    <div className="text-center grid-bg">
-                        <h3 className="no-margin">Aiman Ahmad</h3>
-                        <h3 style={{color: 'gray'}} className="no-margin">Clinician Research Training Fellow</h3>
-                        <div className="pad-md">
-                            <img style={{ width: "35%" }} className="" src={aiman} alt="Aiman Ahmad" />
+
+                    <div className="flex grid-bg">
+                        <div class="marg-left-sm">
+                            <div class="text-center">
+                                <h3>Lauren Harrison-Oakes</h3>
+                                <h4>PhD student</h4>
+                            </div>
+                        </div>
+                        <div className="align-right image-right">
+                            <img className="pad-md people-image" src={lauren} alt="Lauren Harrison-Oakes" />
+                        </div>
+                    </div>
+                    <div className="flex grid-bg">
+                        <div class="marg-left-sm">
+                            <div class="text-center">
+                                <h3>Aiman Ahmad</h3>
+                                <h4>Clinician Research Training Fellow</h4>
+                                <div></div>
+                            </div>
+                        </div>
+                        <div className="align-right image-right">
+                            <img className="pad-md people-image" src={aiman} alt="Aiman Ahmad" />
                         </div>
                     </div>
                 </div>
 
                 <div className="flex-column grid-bg pad-btm-lg marg-top-xl">
-                    <div className='flex diu-text-container'>
-                        <div style={{'max-width': '80%'}}>
+                    <div className='flex marg-left-lg'>
+                        <div>
                             <div className="flex pi">
                                 <div className="flex-column">
                                     <h3 style={{color: 'gray'}}>Past members:</h3>
